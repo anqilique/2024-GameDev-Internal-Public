7c90d7087cfadf6e75964c9fd83c2bd8cc4daa2c
AHulot <107088774+AHulot@users.noreply.github.com>
2024-06-12 19:10:13 +1200

    Chaser Detect Player
    Using a raycast and collision shape for the chaser enemies' vision.

--- scripts/chaser_enemy.gd
+++ scripts/chaser_enemy.gd
@@ -1,31 +1,45 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func check_can_chase(player):
+	if $StateMachine.current_state.name != "EnemyChase":
+		var player_pos = player.global_transform.origin
+		
+		$VisionRayCast3D.look_at(player_pos, Vector3.UP)
+		$VisionRayCast3D.force_raycast_update()
+		
+		if $VisionRayCast3D.is_colliding():
+			var colliding_with = $VisionRayCast3D.get_collider()
+			
+			if colliding_with.name == "Player":
+				print("I see you!")
+				$StateMachine.on_child_transition($StateMachine.current_state, "EnemyChase")
+			else:
+				print("I don't see you!")
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
+	
+	for body in $Vision3D.get_overlapping_bodies():
+		if body.name == "Player":
+			check_can_chase(body)
+
+
+func _on_vision_3d_body_entered(body):
+	if body.name == "Player":
+		if body.get_node("StateMachine").current_state.name != "PlayerDeath":
+			check_can_chase(body)
+
+
+func _on_vision_3d_body_exited(body):
+	if body.name == "Player": # and $StateMachine.current_state.name != "EnemyIdle"
+		if body.get_node("StateMachine").current_state.name != "PlayerDeath":
+			print("Switching to Chase State! I DON'T see you!")
+			$StateMachine.on_child_transition($StateMachine.current_state, "EnemyIdle")
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,25 @@
 extends Node3D
 
 @export var passive_enemy_scene : PackedScene
+@export var chaser_enemy_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_enemies(40)
+	spawn_enemies(0, 1)
 
-func spawn_enemies(how_many):
-	for n in range(how_many):
+func spawn_enemies(how_many_passive, how_many_chaser):
+	for n in range(how_many_passive):
 		var new_enemy = passive_enemy_scene.instantiate()
+		
+		var rand_x = randf_range(-20, 20)
+		var rand_z = randf_range(-20, 20)
+		new_enemy.position = Vector3(rand_x, 0.5, rand_z)
+		
+		new_enemy.add_to_group("Enemies")
+		add_child(new_enemy)
+	
+	for n in range(how_many_chaser):
+		var new_enemy = chaser_enemy_scene.instantiate()
 		
 		var rand_x = randf_range(-20, 20)
 		var rand_z = randf_range(-20, 20)
