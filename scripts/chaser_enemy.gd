fb6097b018734af89d288487fec8ff0a3c4ac4ae
AHulot <107088774+AHulot@users.noreply.github.com>
2024-06-12 19:14:43 +1200

    Chaser Enemy Wander
    Chaser can also wander/patrol around like the passive enemies.

--- scripts/chaser_enemy.gd
+++ scripts/chaser_enemy.gd
@@ -1,7 +1,20 @@
 extends CharacterBody3D
 
+@export var statetimer_min : float
+@export var statetimer_max : float
+
+@onready var time_in_state : float
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	reset_statetimer()
+
+func reset_statetimer():
+	time_in_state = randf_range(statetimer_min, statetimer_max)
+	$StateTimer.wait_time = time_in_state
+	$StateTimer.start()
 
 func check_can_chase(player):
 	if $StateMachine.current_state.name != "EnemyChase":
@@ -30,6 +43,18 @@ func _physics_process(delta):
 	for body in $Vision3D.get_overlapping_bodies():
 		if body.name == "Player":
 			check_can_chase(body)
+
+
+func go_to_new_state(current_state):
+	if current_state == "EnemyIdle":
+		$StateMachine.on_child_transition($StateMachine.current_state, "EnemyWander")
+	elif current_state == "EnemyWander":
+		$StateMachine.on_child_transition($StateMachine.current_state, "EnemyIdle")
+	
+	reset_statetimer()
+
+func _on_state_timer_timeout():
+	go_to_new_state($StateMachine.current_state.name)
 
 
 func _on_vision_3d_body_entered(body):
