1d16277d7e7b6deed4b25e129fa73be6c26a71a6
AHulot <107088774+AHulot@users.noreply.github.com>
2024-07-08 18:12:07 +1200

    Mask Resources
    Started creating resource files for the players' masks.

--- resources/autoloads/player_vars.gd
+++ resources/autoloads/player_vars.gd
@@ -8,11 +8,11 @@ var jump_max_count = 1
 # Mask related variables.
 var current_mask = 5
 var masks = {  # "No. Code" : []
-	1 : [],
-	2 : [],
-	3 : [],
-	4 : [],
-	5 : []
+	1 : ["res://resources/masks/dragon_mask.tres"],
+	2 : ["res://resources/masks/bird_mask.tres"],
+	3 : ["res://resources/masks/tiger_mask.tres"],
+	4 : ["res://resources/masks/tortoise_mask.tres"],
+	5 : ["res://resources/masks/qilin_mask.tres"]
 }
 
 # Health related variables.
--- resources/base_mask.gd
+++ resources/base_mask.gd
@@ -1,3 +1,8 @@
 extends Resource
 
-@export var number = 1
+@export var beast = ""
+@export var direction = ""
+@export var season = ""
+@export var time_of_day = ""
+@export var element = ""
+@export var number = ""
