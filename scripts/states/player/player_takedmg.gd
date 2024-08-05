6e76509c9b9195463a76aab5ffbd5f6d74656e5a
AHulot <107088774+AHulot@users.noreply.github.com>
2024-08-05 19:09:09 +1200

    Implement Better Respawning

--- scripts/states/player/player_takedmg.gd
+++ scripts/states/player/player_takedmg.gd
@@ -1,7 +1,7 @@
 extends State
 class_name PlayerTakeDMG
 
-const DAMAGED_SPEED = 8
+const SLOW_MULTIPLIER = 0.5
 
 var player
 var mask_data
@@ -9,16 +9,17 @@ var mask_data
 func enter():
 	player = get_tree().get_first_node_in_group("Player")
 	player.get_node("RecoveryTimer").start()  # Time spent in this state.
+	
 	mask_data = load(PlayerVars.masks[PlayerVars.current_mask])
 
 func physics_update(_delta):  # Allow the player to move, but slower.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	
 	var movement_speed = PlayerVars.base_speed + mask_data.movement_speed_bonus
 	
-	# Slow the player down when they are hit.
-	player.velocity.x = move_toward(player.velocity.x, direction.x * DAMAGED_SPEED, movement_speed)
-	player.velocity.z = move_toward(player.velocity.z, direction.z * DAMAGED_SPEED, movement_speed)
+	player.velocity.x = direction.x * movement_speed * SLOW_MULTIPLIER
+	player.velocity.z = direction.z * movement_speed * SLOW_MULTIPLIER
 	
 	player.move_and_slide()
